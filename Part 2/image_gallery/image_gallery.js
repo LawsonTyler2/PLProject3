0159f2b07eb3b86fcf0df478227d965c2331c77b
Pavilionall <medel_alvaro@yahoo.com>
2016-03-03 18:34:20 -0600

    Part 2 fini

--- Part 2/image_gallery/image_gallery.js
+++ Part 2/image_gallery/image_gallery.js
@@ -1,20 +1,15 @@
 $(document).ready(function() {
 
-    function preloadImage(url)
-    {
-        var img=new Image();
-        img.src=url;
-    }
-
-    $('#image_list').children('li').each(function() {
-        alert($(this).attr("href"));
-       preloadImage(this.attr("href"));
-
+     // preload images
+    $("#image_list a").each(function(){
+        var swappedImage = new Image();
+        swappedImage.src = $(this).attr("href");
     });
+
     // set up event handlers for links
     $("#image_list a").click(function(evt) {
         // swap image
-        /*var imageURL = $(this).attr("href");
+        var imageURL = $(this).attr("href");
         $("#image").attr("src", imageURL);
 
         //swap caption
@@ -22,13 +17,9 @@ $(document).ready(function() {
         $("#caption").text(caption);
 
         // cancel the default action of the link
-         */
         evt.preventDefault();  // jQuery method that's cross-browser compatible
-
-
-        alert("hello")
     }); // end click
 
     // move focus to first thumbnail
     $("li:first-child a").focus();
-})
+});
